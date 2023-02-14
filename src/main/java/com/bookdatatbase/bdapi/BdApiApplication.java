bb6533f322e2f3e879a11802f07dc1ba5f4be26a
Brian Knight <bknight84@gmail.com>
2023-02-14 09:25:59 -0700

    Builds projects from Spring Initializer

---
+++ src/main/java/com/bookdatatbase/bdapi/BdApiApplication.java
@@ -0,0 +1,13 @@
+package com.bookdatatbase.bdapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookdatatbase/bdapi/BdApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookdatatbase.bdapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
