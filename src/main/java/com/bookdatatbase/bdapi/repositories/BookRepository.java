29b7298490e83d4fe95e1fb320daca727231b127
Brian Knight <bknight84@gmail.com>
2023-02-15 15:52:17 -0700

    Updates BookRepository, BookService, & BookController with UUID id for methods and signatures

--- src/main/java/com/bookdatatbase/bdapi/repositories/BookRepository.java
+++ src/main/java/com/bookdatatbase/bdapi/repositories/BookRepository.java
@@ -4,6 +4,8 @@ import com.bookdatatbase.bdapi.entities.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface BookRepository extends JpaRepository<Book, String> {
+public interface BookRepository extends JpaRepository<Book, UUID> {
 }
