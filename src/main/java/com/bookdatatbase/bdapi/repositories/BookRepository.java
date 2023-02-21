eea152db60ced738626e564f081a2fb6ce4b74b0
Brian Knight <bknight84@gmail.com>
2023-02-21 15:54:20 -0700

    Modifies BookRepo, BookService, & BookController to implement Search & Filter

--- src/main/java/com/bookdatatbase/bdapi/repositories/BookRepository.java
+++ src/main/java/com/bookdatatbase/bdapi/repositories/BookRepository.java
@@ -2,10 +2,12 @@ package com.bookdatatbase.bdapi.repositories;
 
 import com.bookdatatbase.bdapi.entities.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface BookRepository extends JpaRepository<Book, UUID> {
+public interface BookRepository extends JpaRepository<Book, UUID>, JpaSpecificationExecutor<Book> {
+
 }
