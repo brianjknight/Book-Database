8230f206af7a61b0f8afcdbd2d09a5d573036253
Brian Knight <bknight84@gmail.com>
2023-03-07 15:27:58 -0700

    Creates AuthorRepository

--- src/main/java/com/bookdatatbase/bdapi/repositories/AuthorRepository.java
+++ src/main/java/com/bookdatatbase/bdapi/repositories/AuthorRepository.java
@@ -3,6 +3,8 @@ package com.bookdatatbase.bdapi.repositories;
 import com.bookdatatbase.bdapi.entities.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AuthorRepository extends JpaRepository<Author, Integer>, JpaSpecificationExecutor<Author> {
 }
