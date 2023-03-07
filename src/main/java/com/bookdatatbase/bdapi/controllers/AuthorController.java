8790512ba20308cba8281721cae5a68402fba87f
Brian Knight <bknight84@gmail.com>
2023-03-07 15:41:07 -0700

    Creates AuthorController

---
+++ src/main/java/com/bookdatatbase/bdapi/controllers/AuthorController.java
@@ -0,0 +1,41 @@
+package com.bookdatatbase.bdapi.controllers;
+
+import com.bookdatatbase.bdapi.entities.Author;
+import com.bookdatatbase.bdapi.services.AuthorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("authors")
+public class AuthorController {
+    @Autowired
+    private AuthorService authorService;
+
+    @GetMapping("{id}")
+    public ResponseEntity<Author> findAuthorById(@PathVariable Integer id) {
+        return authorService.findAuthorById(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<Author> saveAuthor(@RequestBody Author author) {
+        return authorService.saveAuthor(author);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Author> updateAuthor(@RequestBody Author author) {
+        return authorService.updateAuthor(author);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteAuthorById(@PathVariable Integer id) {
+        return authorService.deleteAuthorById(id);
+    }
+}
