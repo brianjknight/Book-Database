85ac575d6b2c7e3565be279935143b8c9accc480
Brian Knight <bknight84@gmail.com>
2023-03-30 13:53:38 -0600

    Modifies seed limit, commented code for checking existing Books, adds print lines for seeded and not seeded items.

--- src/main/java/com/bookdatatbase/bdapi/dataseeders/AuthorSeeder.java
+++ src/main/java/com/bookdatatbase/bdapi/dataseeders/AuthorSeeder.java
@@ -1,8 +1,10 @@
 package com.bookdatatbase.bdapi.dataseeders;
 
 import com.bookdatatbase.bdapi.entities.Author;
+import com.bookdatatbase.bdapi.entities.Book;
 import com.bookdatatbase.bdapi.json.AuthorDeserializer;
 import com.bookdatatbase.bdapi.services.AuthorService;
+import com.bookdatatbase.bdapi.services.BookService;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,12 +15,15 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Objects;
 import java.util.zip.GZIPInputStream;
 
 @Component
 public class AuthorSeeder implements CommandLineRunner {
     @Autowired
     private AuthorService authorService;
+//    @Autowired
+//    private BookService bookService;
 
     @Override
     public void run(String... args) throws Exception {
@@ -27,8 +32,9 @@ public class AuthorSeeder implements CommandLineRunner {
 
     private void seedAuthorData() {
         if(authorService.count() == 0) {
-            int count = 0;
-            int limit = 100;
+            int seeded = 0;
+            int notSeeded = 0;
+            int limit = 1000;
 
             GsonBuilder builder = new GsonBuilder();
             builder.registerTypeAdapter(Author.class, new AuthorDeserializer());
@@ -42,16 +48,28 @@ public class AuthorSeeder implements CommandLineRunner {
                 String line = reader.readLine();
 
                 while (line != null) {
-                    count++;
                     Author author = gson.fromJson(line, Author.class);
-                    authorService.saveAuthor(author);
 
-                    if (count >= limit) {
+                    authorService.saveAuthor(author);
+                    seeded++;
+
+//                    Book book = bookService.findByAuthorId(author.getAuthorId());
+
+//                    if(Objects.nonNull(book)) {
+//                        seeded++;
+//                        authorService.saveAuthor(author);
+//                    } else {
+//                        notSeeded++;
+//                    }
+
+                    if (seeded >= limit) {
                         break;
                     }
 
                     line = reader.readLine();
                 }
+                System.out.println("Number of Authors persisted to the database = " + seeded);
+                System.out.println("Number of Authors NOT persisted to the database = " +  notSeeded);
             } catch (IOException e) {
                 System.out.println("Could not load author data.");
                 e.printStackTrace();
--- src/main/java/com/bookdatatbase/bdapi/dataseeders/BookGenreSeeder.java
+++ src/main/java/com/bookdatatbase/bdapi/dataseeders/BookGenreSeeder.java
@@ -1,8 +1,10 @@
 package com.bookdatatbase.bdapi.dataseeders;
 
+import com.bookdatatbase.bdapi.entities.Book;
 import com.bookdatatbase.bdapi.entities.BookGenre;
 import com.bookdatatbase.bdapi.json.BookGenreDeserializer;
 import com.bookdatatbase.bdapi.services.BookGenreService;
+import com.bookdatatbase.bdapi.services.BookService;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,12 +15,16 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Objects;
 import java.util.zip.GZIPInputStream;
 
 @Component
 public class BookGenreSeeder implements CommandLineRunner {
     @Autowired
     private BookGenreService bookGenreService;
+
+//    @Autowired
+//    private BookService bookService;
 
     @Override
     public void run(String... args) throws Exception {
@@ -27,8 +33,9 @@ public class BookGenreSeeder implements CommandLineRunner {
 
     private void seedBookGenreData() {
         if(bookGenreService.count() == 0) {
-            int count = 0;
-            int limit = 100;
+            int seeded = 0;
+            int notSeeded = 0;
+            int limit = 1000;
 
             GsonBuilder builder = new GsonBuilder();
             builder.registerTypeAdapter(BookGenre.class, new BookGenreDeserializer());
@@ -42,16 +49,28 @@ public class BookGenreSeeder implements CommandLineRunner {
                 String line = reader.readLine();
 
                 while (line != null) {
-                    count++;
                     BookGenre bookGenre = gson.fromJson(line, BookGenre.class);
-                    bookGenreService.saveBookGenre(bookGenre);
 
-                    if (count >= limit) {
+                    bookGenreService.saveBookGenre(bookGenre);
+                    seeded++;
+
+//                    Book book = bookService.findByBookId(bookGenre.getBookId());
+//
+//                    if(Objects.nonNull(book)) {
+//                        seeded++;
+//                        bookGenreService.saveBookGenre(bookGenre);
+//                    } else {
+//                        notSeeded++;
+//                    }
+
+                    if (seeded >= limit) {
                         break;
                     }
 
                     line = reader.readLine();
                 }
+                System.out.println("Number of BookGenres persisted to the database = " + seeded);
+                System.out.println("Number of BookGenres NOT persisted to the database = " + notSeeded);
             } catch (IOException e) {
                 System.out.println("Could not load book genre data.");
                 e.printStackTrace();
