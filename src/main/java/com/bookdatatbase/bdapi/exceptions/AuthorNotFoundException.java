dc9c763183954cb1baf4ea36970427e539aafe61
Brian Knight <bknight84@gmail.com>
2023-04-26 18:07:25 -0600

    Modifies AuthorNotFoundException to use UUID instead of authorId

--- src/main/java/com/bookdatatbase/bdapi/exceptions/AuthorNotFoundException.java
+++ src/main/java/com/bookdatatbase/bdapi/exceptions/AuthorNotFoundException.java
@@ -1,13 +1,14 @@
 package com.bookdatatbase.bdapi.exceptions;
 
 import java.io.Serial;
+import java.util.UUID;
 
 public class AuthorNotFoundException extends RuntimeException {
 
     @Serial
     private static final long serialVersionUID = -9168882772655282384L;
 
-    public AuthorNotFoundException(Integer authorId) {
-        super("Author with authorId: " + authorId.toString() + " cannot be found");
+    public AuthorNotFoundException(UUID id) {
+        super("Author with authorId: " + id.toString() + " cannot be found");
     }
 }
